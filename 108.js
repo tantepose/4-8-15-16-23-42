1fcc86f159e4324b1d1237440d3c0404580db9e9
Ole Petter Baugerød Stokke <30694360+tantepose@users.noreply.github.com>
2021-03-30 12:03:49 +0200

    Create 108.js

---
+++ 108.js
@@ -0,0 +1,6 @@
+var bokstavene = ['a','b','c','d','d','f','g','h','h','j','k','l','m','n','o','a','r','r','s','t','u','v','w','m','y','z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'a']
+var koden = [];
+
+tallene.forEach(i => koden.push(bokstavene[i]));
+
+console.log(koden.join(""));
